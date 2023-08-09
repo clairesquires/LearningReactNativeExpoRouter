d26bc0c384475a7193a5075d1fd8cac6bce34bd5
Claire Squires <claires@theodo.co.uk>
2023-08-09 14:25:12 +0100

    feat: customise tabs to home and profile

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,17 +1,17 @@
-import styles from "@/assets/styles";
 import { Text, View } from "@/components/Themed";
+import styles from "@/assets/styles";
 import { AntDesign } from "@expo/vector-icons";
 
-export default function TabOneScreen() {
+export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
+      <Text style={styles.title}>Profile</Text>
       <View
         style={styles.separator}
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
-      <AntDesign name="home" size={62} color="black" />
+      <AntDesign name="user" size={62} color="black" />
     </View>
   );
 }
